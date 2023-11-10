af34133734f7b78c796cb7846cb6b8c4c9fe16f7
ADMIN <deathtnt.thebest@gmail.com>
2023-11-10 14:52:30 +0700

    bao up form menu

---
+++ app/src/main/java/com/example/pro1121_nhom3/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.pro1121_nhom3;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.example.pro1121_nhom3.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new newsfeed_Fragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            if(item.getItemId() == R.id.nf) replaceFragment(new newsfeed_Fragment());
+            else if(item.getItemId() == R.id.wl) replaceFragment(new wishlist_Fragment());
+            else if(item.getItemId() == R.id.c) replaceFragment(new cart_Fragment());
+            else if(item.getItemId() == R.id.pf) replaceFragment(new profile_Fragment());
+
+
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment)
+    {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.framelayout, fragment);
+        fragmentTransaction.commit();
+    }
+}
