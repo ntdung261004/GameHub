0e67c1f0ad9779f0df001ea31b8f8a23ceb63463
hnghii <hnghi201003@gmail.com>
2023-11-10 16:30:19 +0700

    git push https://github.com/ntdung261004/GameHub.git

---
+++ app/src/main/java/com/example/pro1121_nhom3/model/BILL.java
@@ -0,0 +1,16 @@
+package com.example.pro1121_nhom3.model;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.pro1121_nhom3.R;
+
+public class BILL extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bill);
+    }
+}
---
+++ app/src/main/java/com/example/pro1121_nhom3/model/Game.java
@@ -0,0 +1,16 @@
+package com.example.pro1121_nhom3.model;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.pro1121_nhom3.R;
+
+public class Game extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
