2abebd282f5fbac066d8e44b9cd978c75e8e01bd
hnghii <hnghi201003@gmail.com>
2023-11-29 08:31:12 +0700

    nạp tiền hiện thông báo

--- app/src/main/java/com/example/pro1121_nhom3/WalletActivity.java
+++ app/src/main/java/com/example/pro1121_nhom3/WalletActivity.java
@@ -1,15 +1,19 @@
 package com.example.pro1121_nhom3;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -23,7 +27,7 @@ public class WalletActivity extends AppCompatActivity {
 
     private TextView sodu;
     private EditText nhaptien;
-    private CardView momo_cardview,atm_cardview,visa_cardview;
+    private CardView momo_cardview, atm_cardview, visa_cardview;
     ImageView btnBack;
 
     @Override
@@ -34,9 +38,9 @@ public class WalletActivity extends AppCompatActivity {
         sodu = findViewById(R.id.sodu);
         nhaptien = findViewById(R.id.nhaptien);
         momo_cardview = findViewById(R.id.momo_cardview);
-        btnBack=findViewById(R.id.btnBack);
-        atm_cardview =findViewById(R.id.atm_cardview);
-        visa_cardview=findViewById(R.id.visa_cardview);
+        btnBack = findViewById(R.id.btnBack);
+        atm_cardview = findViewById(R.id.atm_cardview);
+        visa_cardview = findViewById(R.id.visa_cardview);
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -106,16 +110,42 @@ public class WalletActivity extends AppCompatActivity {
         if (!inputAmount.isEmpty()) {
             double enteredAmount = Double.parseDouble(inputAmount);
 
-            // Lấy số dư hiện tại từ TextView
-            String currentBalanceString = sodu.getText().toString();
-            double currentBalance = Double.parseDouble(currentBalanceString);
+            // Tạo AlertDialog để xác nhận nạp tiền
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Xác nhận nạp tiền");
+            builder.setMessage("Bạn có chắc muốn nạp số tiền này không?");
 
-            // Cộng thêm số tiền nhập vào số dư
-            double updatedBalance = currentBalance + enteredAmount;
+            // Nút "Có"
+            builder.setPositiveButton("Có", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // Lấy số dư hiện tại từ TextView
+                    String currentBalanceString = sodu.getText().toString();
+                    double currentBalance = Double.parseDouble(currentBalanceString);
 
-            // Cập nhật TextView và giá trị ví trên Firebase Realtime Database
-            sodu.setText(String.valueOf(updatedBalance));
-            updateWalletInFirebase(updatedBalance);
+                    // Cộng thêm số tiền nhập vào số dư
+                    double updatedBalance = currentBalance + enteredAmount;
+
+                    // Cập nhật TextView và giá trị ví trên Firebase Realtime Database
+                    sodu.setText(String.valueOf(updatedBalance));
+                    updateWalletInFirebase(updatedBalance);
+
+                    // Hiển thị thông báo nạp tiền thành công
+                    Toast.makeText(WalletActivity.this, "Nạp tiền thành công", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            // Nút "Không"
+            builder.setNegativeButton("Không", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // Đóng AlertDialog nếu người dùng chọn "Không"
+                    dialog.dismiss();
+                }
+            });
+
+            // Hiển thị AlertDialog
+            builder.create().show();
         }
     }
 
@@ -165,6 +195,4 @@ public class WalletActivity extends AppCompatActivity {
             });
         }
     }
-
-
 }
