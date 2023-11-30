67084a4603703180eacb9f7b027172a61f0ec532
ntdung261004 <ntdung261004@gmail.com>
2023-11-30 19:50:53 +0700

    done xoa game, add game: spinner done, lấy ảnh done, chưa bt put ảnh để add.

--- app/src/main/java/com/example/pro1121_nhom3/ThemGameActivity.java
+++ app/src/main/java/com/example/pro1121_nhom3/ThemGameActivity.java
@@ -1,9 +1,15 @@
 package com.example.pro1121_nhom3;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -15,13 +21,23 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.pro1121_nhom3.model.loaigame;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class ThemGameActivity extends AppCompatActivity {
 
@@ -32,22 +48,27 @@ public class ThemGameActivity extends AppCompatActivity {
     private EditText edtTenGame, edtNPH, edtGia, edtNgayPH, edtLikeCount, edtSellCount, edtMoTa;
     private Button btnAdd, btnCancel, btnThemHinhAnhSP;
     private DatabaseReference databaseReference;
+    private DatabaseReference root = FirebaseDatabase.getInstance().getReference("game");
+    private StorageReference sReference = FirebaseStorage.getInstance().getReference();
+
+    private Uri imageURI;
+    String img = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_them_game);
 
-        ImageView ivSanPhamMoi = findViewById(R.id.ivSanPhamMoi);
-        Button btnThemHinhAnhSP = findViewById(R.id.btnThemHinhAnhSP);
-        EditText edtTenGame = findViewById(R.id.edtTenGame);
+        ivSanPhamMoi = findViewById(R.id.ivSanPhamMoi);
+        btnThemHinhAnhSP = findViewById(R.id.btnThemHinhAnhSP);
+        edtTenGame = findViewById(R.id.edtTenGame);
         SpnMaLoai = findViewById(R.id.SpnMaLoaiActivity);
-        EditText edtNPH = findViewById(R.id.edtNPH);
-        EditText edtGia = findViewById(R.id.edtGia);
-        EditText edtNgayPH = findViewById(R.id.edtNgayPH);
-        EditText edtMoTa = findViewById(R.id.edtMoTa);
-        EditText edtLikeCount = findViewById(R.id.edtLikeCount);
-        EditText edtSellCount = findViewById(R.id.edtSellCount);
+        edtNPH = findViewById(R.id.edtNPH);
+        edtGia = findViewById(R.id.edtGia);
+        edtNgayPH = findViewById(R.id.edtNgayPH);
+        edtMoTa = findViewById(R.id.edtMoTa);
+        edtLikeCount = findViewById(R.id.edtLikeCount);
+        edtSellCount = findViewById(R.id.edtSellCount);
 
         Button btnAdd = findViewById(R.id.btnAdd);
         Button btnCancel = findViewById(R.id.btnCancel);
@@ -111,6 +132,47 @@ public class ThemGameActivity extends AppCompatActivity {
         });
 
 
+        btnThemHinhAnhSP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                choosePicture();
+            }
+        });
+
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Map<String,Object> map = new HashMap<>();
+
+                map.put("tengame",edtTenGame.getText().toString());
+                map.put("loaigame",SpnMaLoai.getSelectedItem().toString());
+                map.put("nph", edtNPH.getText().toString());
+                map.put("giaban", edtGia.getText().toString());
+                map.put("ngayph", edtNgayPH.getText().toString());
+                map.put("mota", edtMoTa.getText().toString());
+                map.put("likecount", edtLikeCount.getText().toString());
+                map.put("sellcount", edtSellCount.getText().toString());
+
+                FirebaseDatabase.getInstance().getReference().child("game").push()
+                        .setValue(map)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Toast.makeText(ThemGameActivity.this, "Game Added Successfully!", Toast.LENGTH_SHORT).show();
+                                finish();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(ThemGameActivity.this, "Error while Adding", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
+
+
+
     }
 
     private void fetchDataFromFirebase() {
@@ -150,6 +212,120 @@ public class ThemGameActivity extends AppCompatActivity {
             //log de biet bi loi gi
             Log.d("MYLOG","Loi: "+ e);
         }
+    }
+
+    private void choosePicture() {
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+
+        Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // inten tich hop 2 intenr tren 1  hop
+        Intent tuychon = Intent.createChooser(i, "chọn 1 mục");
+        tuychon.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[]{camera});
+        startActivityForResult(tuychon,999);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==999 && resultCode==RESULT_OK){
+            if (data.getExtras() != null) {// lay tu camera
+                Bundle b = data.getExtras();
+                Bitmap bm = (Bitmap) b.get("data");
+                ivSanPhamMoi.setImageBitmap(bm);
+                uploadBitmap(bm);
+            }else {
+                imageURI = data.getData();
+                ivSanPhamMoi.setImageURI(imageURI);
+                uploadPicture();
+            }
+        }
+    }
+
+    private void uploadBitmap(Bitmap bm) {
+        final ProgressDialog dialog = new ProgressDialog(this);
+        dialog.setTitle("Đang tải...");
+        dialog.show();
+        final long id = System.currentTimeMillis();
+        StorageReference river = sReference.child("image/"+id);
+
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bm.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        byte[] byteArray = stream.toByteArray();
+        river.putBytes(byteArray)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        river.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                dialog.dismiss();
+                                img = uri+"";
+                            }
+                        });
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        dialog.dismiss();
+                        Toast.makeText(getApplicationContext(), "Tải không thành công.", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                        dialog.setMessage("Đang tải... ");
+                    }
+                });
+    }
+
+    private void uploadPicture() {
+        final ProgressDialog dialog = new ProgressDialog(this);
+        dialog.setTitle("Đang tải...");
+        dialog.show();
+        final String randomKey = UUID.randomUUID().toString();
+        StorageReference river = sReference.child("image/*"+randomKey);
+
+        river.putFile(imageURI)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        dialog.dismiss();
+                        river.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                img = uri+"";
+                            }
+                        });
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        dialog.dismiss();
+                        Toast.makeText(getApplicationContext(), "Tải không thành công.", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                        dialog.setMessage("Đang tải... ");
+                    }
+                });
+    }
+
+    private void clearAll(){
+        edtTenGame.setText("");
+        edtNPH.setText("");
+        edtMoTa.setText("");
+        edtGia.setText("");
+        edtNgayPH.setText("");
+        edtSellCount.setText("");
+        edtLikeCount.setText("");
+        ivSanPhamMoi.setImageResource(R.mipmap.auto);
     }
 
 }
