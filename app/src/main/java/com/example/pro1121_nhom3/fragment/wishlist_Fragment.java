bf85d62b9ef038a54d7dc1720ac7092ae57989f2
ADMIN <deathtnt.thebest@gmail.com>
2023-11-30 15:59:31 +0700

    wishlist

--- app/src/main/java/com/example/pro1121_nhom3/fragment/wishlist_Fragment.java
+++ app/src/main/java/com/example/pro1121_nhom3/fragment/wishlist_Fragment.java
@@ -3,64 +3,48 @@ package com.example.pro1121_nhom3.fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.pro1121_nhom3.LibraryActivity;
 import com.example.pro1121_nhom3.R;
+import com.example.pro1121_nhom3.adapter.gameLibAdapter;
+import com.example.pro1121_nhom3.model.game;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link wishlist_Fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
 public class wishlist_Fragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    RecyclerView rcvLib;
+    ArrayList<game> listgame;
 
     public wishlist_Fragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment wishlist_Fragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static wishlist_Fragment newInstance(String param1, String param2) {
-        wishlist_Fragment fragment = new wishlist_Fragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_wishlist_, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_wishlist_, container, false);
+        rcvLib = view.findViewById(R.id.rcvwishlist);
+        listgame = new ArrayList<>();
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), 4);
+        gameLibAdapter libAdapter = new gameLibAdapter(listgame, getActivity());
+        libAdapter.getLibList(listgame);
+        rcvLib.setAdapter(libAdapter);
+        rcvLib.setLayoutManager(gridLayoutManager);
+
+
+
+
+        return view;
     }
 }
