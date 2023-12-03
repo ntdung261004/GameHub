a186582c5896ff7b0c8e82d204f71312e6b59bb8
hnghii <hnghi201003@gmail.com>
2023-12-04 01:42:56 +0700

    fix lỏ

--- app/src/main/java/com/example/pro1121_nhom3/pagegameActivity.java
+++ app/src/main/java/com/example/pro1121_nhom3/pagegameActivity.java
@@ -91,7 +91,7 @@ public class pagegameActivity extends AppCompatActivity {
                         tvngayph.setText(gameIndex.getNgayph());
                         tvmota.setText(gameIndex.getMota());
                         tvnph.setText(gameIndex.getNph());
-                        tvsellcount.setText((int)gameIndex.getSellcount() + " VND");
+                        tvsellcount.setText((int)gameIndex.getSellcount());
                         tvlikecount.setText(gameIndex.getLikecount()+"");
                         Glide.with(pagegameActivity.this).load(gameIndex.getImg()).into(banner);
 
@@ -111,6 +111,7 @@ public class pagegameActivity extends AppCompatActivity {
 
             }
         });
+
 
         btbuy.setOnClickListener(new View.OnClickListener() {
             @Override
