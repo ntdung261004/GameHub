01fee505f116c10eded00d11befa88d86cdd2106
hnghii <hnghi201003@gmail.com>
2023-12-07 18:43:34 +0700

    xong admin dổi mk vs logout, đéo search đc

--- app/src/main/java/com/example/pro1121_nhom3/GameActivity.java
+++ app/src/main/java/com/example/pro1121_nhom3/GameActivity.java
@@ -1,5 +1,10 @@
 package com.example.pro1121_nhom3;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,43 +12,41 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.SearchView;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import com.example.pro1121_nhom3.adapter.AdminGameAdapter;
+import com.example.pro1121_nhom3.adapter.gameLibAdapter;
 import com.example.pro1121_nhom3.model.game;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class GameActivity extends AppCompatActivity {
     ImageView ivMenuBack;
     FloatingActionButton floatAdd;
     private RecyclerView recyclerviewgame;
+    ArrayList<game> listgame;
     private SearchView searchView;
     private AdminGameAdapter adminGameAdapter;
+
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-
         floatAdd = findViewById(R.id.floatAdd);
         ivMenuBack = findViewById(R.id.ivMenuBack);
+        listgame = new ArrayList<>();
         searchView  = findViewById(R.id.searchView);
         recyclerviewgame = findViewById(R.id.recyclerviewgame);
-
         recyclerviewgame.setLayoutManager(new LinearLayoutManager(this));
-
         FirebaseRecyclerOptions<game> options = new FirebaseRecyclerOptions.Builder<game>()
                 .setQuery(FirebaseDatabase.getInstance().getReference().child("game"), game.class)
                 .build();
-
         adminGameAdapter = new AdminGameAdapter(options, this);
-        recyclerviewgame.setAdapter(adminGameAdapter);
-
-        // Set up SearchView listener
+        listgame = new ArrayList<>();
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
@@ -52,13 +55,13 @@ public class GameActivity extends AppCompatActivity {
 
             @Override
             public boolean onQueryTextChange(String newText) {
+                adminGameAdapter.filterByName(newText);
                 Log.d("SearchView", "Query text changed to: " + newText);
-                adminGameAdapter.getFilter().filter(newText);
+                adminGameAdapter.filterByName(newText);
                 return true;
             }
         });
-
-        // Back button click listener
+        recyclerviewgame.setAdapter(adminGameAdapter);
         ivMenuBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -66,7 +69,6 @@ public class GameActivity extends AppCompatActivity {
             }
         });
 
-        // Floating action button click listener
         floatAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -74,6 +76,8 @@ public class GameActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+
     }
 
     @Override
@@ -87,4 +91,5 @@ public class GameActivity extends AppCompatActivity {
         super.onStop();
         adminGameAdapter.stopListening();
     }
+
 }
--- app/src/main/java/com/example/pro1121_nhom3/adapter/AdminGameAdapter.java
+++ app/src/main/java/com/example/pro1121_nhom3/adapter/AdminGameAdapter.java
@@ -1,63 +1,66 @@
 package com.example.pro1121_nhom3.adapter;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Filter;
-import android.widget.Filterable;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.pro1121_nhom3.R;
 import com.example.pro1121_nhom3.UpdateGameActivity;
 import com.example.pro1121_nhom3.model.game;
-import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.FirebaseDatabase;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.orhanobut.dialogplus.DialogPlus;
+import com.orhanobut.dialogplus.ViewHolder;
 import com.squareup.picasso.Picasso;
 
 import java.text.NumberFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
-public class AdminGameAdapter extends FirebaseRecyclerAdapter<game, AdminGameAdapter.admingameViewHolder> implements Filterable {
+public class AdminGameAdapter extends FirebaseRecyclerAdapter<game, AdminGameAdapter.admingameViewHolder>{
+
     private List<game> originalList;
     private List<game> filteredList;
     private Context c;
-
     public AdminGameAdapter(@NonNull FirebaseRecyclerOptions<game> options, Context c) {
         super(options);
         this.c = c;
         filteredList = new ArrayList<>();
         originalList = new ArrayList<>();
     }
-
     @Override
-    protected void onBindViewHolder(@NonNull admingameViewHolder holder, int position, @NonNull game model) {
-        String key = getRef(position).getKey();
+    protected void onBindViewHolder(@NonNull admingameViewHolder holder, @SuppressLint("RecyclerView") final int position, @NonNull game model) {
+        String key = getSnapshots().getSnapshot(position).getKey();
         model.setMagame(key);
-
         String imgGame = model.getImg();
         if (filteredList.size() > 0 && position < filteredList.size()) {
             String keyy = filteredList.get(position).getMagame();
             model.setMagame(key);
         }
-
         if (imgGame != null && !imgGame.isEmpty()) {
             Picasso.get().load(imgGame).into(holder.imgGame);
         } else {
-            // Xử lý trường hợp ảnh rỗng hoặc null
+
         }
 
         holder.tvAdminTengame.setText(model.getTengame());
@@ -70,7 +73,25 @@ public class AdminGameAdapter extends FirebaseRecyclerAdapter<game, AdminGameAda
         holder.ivXoa.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                showDeleteDialog(key);
+                AlertDialog.Builder builder = new AlertDialog.Builder(holder.tvAdminTengame.getContext());
+                builder.setTitle("Xoá game");
+                builder.setMessage("Hành động này không thể hoàn tác");
+
+                builder.setPositiveButton("Xoá", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        FirebaseDatabase.getInstance().getReference().child("game")
+                                .child(getRef(position).getKey()).removeValue();
+                    }
+                });
+
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(holder.tvAdminTengame.getContext(), "Cancelled", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                builder.show();
             }
         });
 
@@ -89,7 +110,10 @@ public class AdminGameAdapter extends FirebaseRecyclerAdapter<game, AdminGameAda
                 c.startActivity(i);
             }
         });
+        // Add the game to the original list
+        originalList.add(model);
     }
+
 
     @NonNull
     @Override
@@ -98,97 +122,29 @@ public class AdminGameAdapter extends FirebaseRecyclerAdapter<game, AdminGameAda
         return new admingameViewHolder(v);
     }
 
-    @Override
-    public int getItemCount() {
-        return filteredList.size();
-    }
-
-    class admingameViewHolder extends RecyclerView.ViewHolder {
+    class admingameViewHolder extends RecyclerView.ViewHolder{
         private ImageView imgGame;
         private TextView tvAdminTengame, tvAdminLoaigame, tvAdminNph, tvAdminGiaban;
-        private ImageView ivSua, ivXoa;
 
+        private ImageView ivSua, ivXoa;
         public admingameViewHolder(@NonNull View itemView) {
             super(itemView);
-            imgGame = itemView.findViewById(R.id.game_img);
-            tvAdminTengame = itemView.findViewById(R.id.tvAdminTengame);
-            tvAdminLoaigame = itemView.findViewById(R.id.tvAdminLoaigame);
-            tvAdminNph = itemView.findViewById(R.id.tvAdminNph);
-            tvAdminGiaban = itemView.findViewById(R.id.tvAdminGiaban);
-            ivSua = itemView.findViewById(R.id.ivSua);
-            ivXoa = itemView.findViewById(R.id.ivXoa);
+            imgGame = (ImageView) itemView.findViewById(R.id.game_img);
+            tvAdminTengame = (TextView) itemView.findViewById(R.id.tvAdminTengame);
+            tvAdminLoaigame = (TextView) itemView.findViewById(R.id.tvAdminLoaigame);
+            tvAdminNph = (TextView) itemView.findViewById(R.id.tvAdminNph);
+            tvAdminGiaban = (TextView) itemView.findViewById(R.id.tvAdminGiaban);
+            ivSua = (ImageView) itemView.findViewById(R.id.ivSua);
+            ivXoa = (ImageView) itemView.findViewById(R.id.ivXoa);
         }
     }
-
-    @Override
-    public Filter getFilter() {
-        return gameFilter;
-    }
-
-    private Filter gameFilter = new Filter() {
-        @Override
-        protected FilterResults performFiltering(CharSequence constraint) {
-            List<game> filteredResults = new ArrayList<>();
-            if (constraint == null || constraint.length() == 0) {
-                filteredResults.addAll(originalList);
-            } else {
-                String filterPattern = constraint.toString().toLowerCase().trim();
-                for (game item : originalList) {
-                    if (item.getTengame().toLowerCase().contains(filterPattern)) {
-                        filteredResults.add(item);
-                    }
-                }
+    public void filterByName(String name) {
+        filteredList.clear();
+        for (game model : originalList) {
+            if (model.getTengame().toLowerCase().contains(name.toLowerCase())) {
+                filteredList.add(model);
             }
-            FilterResults results = new FilterResults();
-            results.values = filteredResults;
-            return results;
         }
-
-        @Override
-        protected void publishResults(CharSequence constraint, FilterResults results) {
-            filteredList.clear();
-            filteredList.addAll((List<game>) results.values);
-            notifyDataSetChanged();
-        }
-    };
-
-    private void showDeleteDialog(String key) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(c);
-        builder.setTitle("Xoá game");
-        builder.setMessage("Hành động này không thể hoàn tác");
-
-        builder.setPositiveButton("Xoá", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                FirebaseDatabase.getInstance().getReference().child("game").child(key).removeValue()
-                        .addOnSuccessListener(new OnSuccessListener<Void>() {
-                            @Override
-                            public void onSuccess(Void unused) {
-                                Toast.makeText(c, "Đã xoá", Toast.LENGTH_SHORT).show();
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Toast.makeText(c, "Xoá thất bại", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-            }
-        });
-
-        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                Toast.makeText(c, "Cancelled", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        builder.show();
-    }
-
-    // Cập nhật originalList khi dữ liệu thay đổi
-    public void setOriginalList(List<game> originalList) {
-        this.originalList = originalList;
         notifyDataSetChanged();
     }
 }
